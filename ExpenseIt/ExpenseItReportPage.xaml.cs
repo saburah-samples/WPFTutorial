bbce7ac2f551d6d9eb2a0db4bb8d60aa14a50e67
saburah-samples <saburah2@gmail.com>
2014-05-21 00:07:28 +0400

    initial commit
    application ExpenseIt (see:
    "http://msdn.microsoft.com/en-us/library/ms752299(v=vs.110).aspx")

---
+++ ExpenseIt/ExpenseItReportPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItReportPage.xaml
+    /// </summary>
+    public partial class ExpenseItReportPage : Page
+    {
+        public ExpenseItReportPage()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// Custom constructor to pass expense report data
+        /// </summary>
+        /// <param name="data">data - expense report data</param>
+        public ExpenseItReportPage(object data)
+            :this()
+        {
+            // Bind to expense report data
+            this.DataContext = data;
+        }
+    }
+}
