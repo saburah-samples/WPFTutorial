9b3f6f6a88c5bdd6c37eb12420242ce0d3903232
saburah-samples <saburah2@gmail.com>
2014-05-27 00:56:06 +0400

    HelloMvvm - initial commit

---
+++ HelloMvvm/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,60 @@
+﻿using HelloMvvm.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HelloMvvm.ViewModels
+{
+    public class PeopleViewModel: ViewModel
+    {
+        public PeopleViewModel()
+        {
+            People = new ObservableCollection<Person>()
+            {
+                new Person { FirstName="Tom", LastName="Jones", Age=80 }, 
+                new Person { FirstName="Dick", LastName="Tracey", Age=40 }, 
+                new Person { FirstName="Harry", LastName="Hill", Age=60 }, 
+            };
+
+            AddPersonCommand = new RelayCommand(AddPerson);
+        }
+
+        public ICommand AddPersonCommand { get; set; }
+
+        public ObservableCollection<Person> People { get; set; }
+        
+        public Person SelectedPerson
+        {
+            get
+            {
+                return this.selectedPerson;
+            }
+            set
+            {
+                if (this.selectedPerson != value)
+                {
+                    RaisePropertyChanging("SelectedPerson");
+                    this.selectedPerson = value;
+                    RaisePropertyChanged("SelectedPerson");
+                }
+            }
+        }
+        private Person selectedPerson;
+
+        private void AddPerson(object parameter)
+        {
+            if (parameter != null)
+            {
+                People.Add(new Person() { FirstName = parameter.ToString(), LastName = parameter.ToString(), Age = DateTime.Now.Second });
+            }
+            else
+            {
+                MessageBox.Show("parameter == null");
+            }
+        }
+    }
+}
---
+++ HelloMvvm/ViewModels/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace HelloMvvm.ViewModels
+{
+    public abstract class ViewModel: INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        #region INotifyPropertyChanged implementation
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (!string.IsNullOrWhiteSpace(propertyName) && PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        } 
+        
+        #endregion
+
+        #region INotifyPropertyChanging implementation
+        
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected void RaisePropertyChanging(string propertyName)
+        {
+            if (!string.IsNullOrWhiteSpace(propertyName) && PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
