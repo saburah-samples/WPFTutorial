a760005c55f65b8b16a1713c499e269e3fd7a55f
Denis_Vakhrushev <saburah2@gmail.com>
2014-05-31 21:42:40 +0400

    HelloMvvm - added navigation between view models

---
+++ HelloMvvm/ViewModels/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace HelloMvvm.ViewModels
+{
+    public class HomeViewModel : ViewModel
+    {
+        public HomeViewModel()
+        {
+            ShowPeopleCommand = new RelayCommand(ShowPeople);
+        }
+        public string Title { get { return "Home"; } }
+        public ICommand ShowPeopleCommand { get; private set; }
+
+        private void ShowPeople(object parameter)
+        {
+            var model = new PeopleViewModel();
+        }
+    }
+}
--- HelloMvvm/Views/HomeView.xaml.cs
+++ HelloMvvm/Views/HomeView.xaml.cs
@@ -1,9 +1,7 @@
-﻿using HelloMvvm.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,14 +12,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HelloMvvm
+namespace HelloMvvm.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomeView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomeView : UserControl
     {
-        public MainWindow()
+        public HomeView()
         {
             InitializeComponent();
         }
