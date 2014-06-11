0eaffc07f9b3874f1abe45b7a4c729537481cc06
Denis_Vakhrushev <saburah2@gmail.com>
2014-06-11 17:37:45 +0400

    HelloEF - initial commit

---
+++ HelloEF/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,64 @@
+﻿using HelloEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloEF.ViewModels
+{
+    public class CustomerViewModel : ViewModelBase
+    {
+        private List<Customer> _customers;
+        private Customer _currentCustomer;
+        private CustomerRepository _repository;
+
+        public CustomerViewModel()
+        {
+            _repository = new CustomerRepository();
+            _customers = _repository.GetCustomers();
+
+            WireCommands();
+        }
+
+        private void WireCommands()
+        {
+            UpdateCustomerCommand = new RelayCommand(UpdateCustomer);
+        }
+
+        public RelayCommand UpdateCustomerCommand
+        {
+            get;
+            private set;
+        }
+
+        public List<Customer> Customers
+        {
+            get { return _customers; }
+            set { _customers = value; }
+        }
+
+        public Customer CurrentCustomer
+        {
+            get
+            {
+                return _currentCustomer;
+            }
+
+            set
+            {
+                if (_currentCustomer != value)
+                {
+                    _currentCustomer = value;
+                    OnPropertyChanged("CurrentCustomer");
+                    UpdateCustomerCommand.IsEnabled = true;
+                }
+            }
+        }
+
+        public void UpdateCustomer()
+        {
+            _repository.UpdateCustomer(CurrentCustomer);
+        }
+    }
+}
---
+++ HelloEF/readme.txt
@@ -0,0 +1,8 @@
+﻿LEARN: Cross-Platform Development with the Portable Class Library
+	http://msdn.microsoft.com/en-us/library/windows/apps/gg597391.aspx
+	http://msdn.microsoft.com/ru-ru/library/windows/apps/hh563947.aspx
+	TODO: extract Models & ViewModels into partable class library
+
+LEARN: Using Repository and Unit of Work patterns with Entity Framework 4.0
+	http://blogs.msdn.com/b/adonet/archive/2009/06/16/using-repository-and-unit-of-work-patterns-with-entity-framework-4-0.aspx
+	TODO: implement repository & unit-of-work
